3ab4d0ed376c15199d94eacd68ac2bf42fad8dba
WeezerIrony <leo@boyandin.ru>
2022-01-09 20:53:23 +0700

    aefefdgd

--- server/server.h
+++ server/server.h
@@ -5,14 +5,15 @@
  * You should have received a copy of the MIT license with
  * this file. If not, please write to: bezborodoff.gleb@gmail.com, or visit : https://github.com/ElectronRock/etherium-chat
  */
-
+#include <map>
+#include <set>
 #include "proto/message_server.grpc.pb.h"
 
 namespace net {
 
     class server final : public message_server_api::storage::Service {
     public:
-        virtual ~server() override = default;
+        virtual ~server() override {};
 
         virtual ::grpc::Status add_message(::grpc::ServerContext* context, 
             const ::message_server_api::add_message_request* request, ::message_server_api::add_message_response* response) override;
@@ -22,6 +23,15 @@ namespace net {
 
         virtual ::grpc::Status register_client(::grpc::ServerContext* context, 
             const ::message_server_api::register_client_request* request, ::message_server_api::register_client_response* response) override;
+    private:
+        struct message final {
+            message(unsigned int u=0, std::string s=""): client_id(u), msg_text(s) {};
+            unsigned int client_id;
+            std::string msg_text;
+        };
+        using storage = std::vector<message>;
+        storage m_storage;
+        std::set<unsigned int> m_clients;
     };
 
 }
